e0dfc51e713c2540f55fa23cb7549e629208dd7b
DeviousMalcontent <deviousmalcontent@gmail.com>
2026-05-25 22:31:32 +1000

    Offline build of xcibyl-translator
    The binary file is right there so now there's no reason for it to not be included in the package.

---
+++ CMakeFiles/xcibyl-translator.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_abbrev_hash.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_abbrev_hash.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_abbrev_hash.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_attr.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_attr.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_attr.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_attr_integrate.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_attr_integrate.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_attr_integrate.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_begin_elf.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_begin_elf.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_begin_elf.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_child.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_child.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_child.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_end.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_end.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_end.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_error.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_error.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_error.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_formaddr.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formaddr.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formaddr.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_formref.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formref.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formref.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_formref_die.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formref_die.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formref_die.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_formudata.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formudata.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_formudata.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_getabbrev.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_getabbrev.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_getabbrev.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_hasattr.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_hasattr.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_hasattr.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_hasattr_integrate.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_hasattr_integrate.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_hasattr_integrate.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_lowpc.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_lowpc.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_lowpc.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_nextcu.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_nextcu.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_nextcu.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_offdie.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_offdie.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_offdie.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_siblingof.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_siblingof.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_siblingof.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_sig8_hash.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_sig8_hash.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_sig8_hash.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/dwarf_tag.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_tag.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/dwarf_tag.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/frame-cache.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/frame-cache.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/frame-cache.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/libdw_alloc.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_alloc.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_alloc.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/libdw_findcu.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_findcu.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_findcu.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/external/elfutils/libdw_form.c" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_form.o" "gcc" "CMakeFiles/xcibyl-translator.dir/external/elfutils/libdw_form.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/mips-dwarf.c" "CMakeFiles/xcibyl-translator.dir/mips-dwarf.o" "gcc" "CMakeFiles/xcibyl-translator.dir/mips-dwarf.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/basicblock.cc" "CMakeFiles/xcibyl-translator.dir/basicblock.o" "gcc" "CMakeFiles/xcibyl-translator.dir/basicblock.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/builtins.cc" "CMakeFiles/xcibyl-translator.dir/builtins.o" "gcc" "CMakeFiles/xcibyl-translator.dir/builtins.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/calltablemethod.cc" "CMakeFiles/xcibyl-translator.dir/calltablemethod.o" "gcc" "CMakeFiles/xcibyl-translator.dir/calltablemethod.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/codeblock.cc" "CMakeFiles/xcibyl-translator.dir/codeblock.o" "gcc" "CMakeFiles/xcibyl-translator.dir/codeblock.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/controller.cc" "CMakeFiles/xcibyl-translator.dir/controller.o" "gcc" "CMakeFiles/xcibyl-translator.dir/controller.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/elf.cc" "CMakeFiles/xcibyl-translator.dir/elf.o" "gcc" "CMakeFiles/xcibyl-translator.dir/elf.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/emit.cc" "CMakeFiles/xcibyl-translator.dir/emit.o" "gcc" "CMakeFiles/xcibyl-translator.dir/emit.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/function.cc" "CMakeFiles/xcibyl-translator.dir/function.o" "gcc" "CMakeFiles/xcibyl-translator.dir/function.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/functioncolocation.cc" "CMakeFiles/xcibyl-translator.dir/functioncolocation.o" "gcc" "CMakeFiles/xcibyl-translator.dir/functioncolocation.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/instruction.cc" "CMakeFiles/xcibyl-translator.dir/instruction.o" "gcc" "CMakeFiles/xcibyl-translator.dir/instruction.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/javaclass.cc" "CMakeFiles/xcibyl-translator.dir/javaclass.o" "gcc" "CMakeFiles/xcibyl-translator.dir/javaclass.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/javamethod.cc" "CMakeFiles/xcibyl-translator.dir/javamethod.o" "gcc" "CMakeFiles/xcibyl-translator.dir/javamethod.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/mips.cc" "CMakeFiles/xcibyl-translator.dir/mips.o" "gcc" "CMakeFiles/xcibyl-translator.dir/mips.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/registerallocator.cc" "CMakeFiles/xcibyl-translator.dir/registerallocator.o" "gcc" "CMakeFiles/xcibyl-translator.dir/registerallocator.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/string-instruction.cc" "CMakeFiles/xcibyl-translator.dir/string-instruction.o" "gcc" "CMakeFiles/xcibyl-translator.dir/string-instruction.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/syscall-wrappers.cc" "CMakeFiles/xcibyl-translator.dir/syscall-wrappers.o" "gcc" "CMakeFiles/xcibyl-translator.dir/syscall-wrappers.o.d"
+  "/mnt/c/Users/Mark/Documents/GitHub/cibyl/tools/translator/utils.cc" "CMakeFiles/xcibyl-translator.dir/utils.o" "gcc" "CMakeFiles/xcibyl-translator.dir/utils.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
